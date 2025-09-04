c2da11c0c96cd185c2f6005cfc5b10e2ce42dd9a
sundramjatav123 <sundram.jatav@redvisionglobal.com>
2025-09-04 17:21:27 +0530

    add

--- src/app/(main)/tools/calculators/page.jsx
+++ src/app/(main)/tools/calculators/page.jsx
@@ -1,17 +1,12 @@
-import { Suspense } from "react";
-import InnerBanner from "@/components/InnerBanner/InnerBanner";
+"use client"
 import AllCalculator from "../../calculators/page";
 
 
 export default function Page() {
   return (
     <div>
-      {/* <InnerBanner title="Financial Calculator" />
-      <div className="max-w-screen-xl mx-auto main_section">
-        <Suspense fallback={<div>Loading calculators...</div>}> */}
           <AllCalculator />
-        {/* </Suspense>
-      </div> */}
+      
     </div>
   );
 }
